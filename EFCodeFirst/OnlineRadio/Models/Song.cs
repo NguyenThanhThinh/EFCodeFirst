60c1a2748741a39865f8ffda7c7ab5b26e246f67
thanhthinhcntt <thanhthinhcntt@gmail.com>
2017-10-30 15:47:26 +0700

    add file

---
+++ EFCodeFirst/OnlineRadio/Models/Song.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace OnlineRadio.Models
+{
+    using OnlineRadio.Utilities;
+    public class Song
+    {
+        private int seconds;
+        private int minutes;
+        private string artistName;
+        private string songName;
+
+        public Song(string artistName, string songName, int minutes, int seconds)
+        {
+            this.ArtistName = artistName;
+            this.SongName = songName;
+            this.Minutes = minutes;
+            this.Seconds = seconds;
+        }
+        public int Seconds
+        {
+            //C# 7.0
+            get => seconds;
+            set
+            {
+                if (value < 0 || value > 59)
+                {
+                    throw new ArgumentException(ErrorMessages.InvalidSongSeconds);
+                }
+                seconds = value;
+            }
+
+        }
+        public int Minutes
+        {
+            get => minutes;
+
+            set
+            {
+                if (value < 0 || value > 14)
+                {
+                    throw new ArgumentException(ErrorMessages.InvalidSongMinutes);
+                }
+                minutes = value;
+            }
+        }
+        public string ArtistName
+        {
+            get => artistName;
+
+            set
+            {
+                if (value.Length < 3 || value.Length > 20)
+                {
+                    throw new ArgumentException(ErrorMessages.InvalidArtistName);
+                }
+
+                artistName = value;
+            }
+        }
+        public string SongName
+        {
+            get => songName;
+
+            set
+            {
+                if (value.Length < 3 || value.Length > 30)
+                {
+                    throw new ArgumentException(ErrorMessages.InvalidSongLength);
+                }
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException(ErrorMessages.InvalidSongName);
+                }
+
+                songName = value;
+            }
+        }
+    }
+}
---
+++ EFCodeFirst/OnlineRadio/Utilities/ErrorMessages.cs
@@ -0,0 +1,12 @@
+﻿namespace OnlineRadio.Utilities
+{
+    public class ErrorMessages
+    {
+        public const string InvalidSong = "Invalid song.";
+        public const string InvalidSongLength = "Invalid song length.";
+        public const string InvalidSongMinutes = "Song minutes should be between 0 and 14.";
+        public const string InvalidSongName = "Song name should be between 3 and 30 symbols.";
+        public const string InvalidArtistName = "Artist name should be between 3 and 20 symbols.";
+        public const string InvalidSongSeconds = "Song seconds should be between 0 and 59.";
+    }
+}
